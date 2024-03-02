1d299de12d54ab4785b56aeb508c98bae1750cdb
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-03-02 12:46:04 +0300

    Добавьте файлы проекта.

---
+++ HalfSorting.cpp
@@ -0,0 +1,47 @@
+﻿// Написать функцию, которая сортирует первую
+// половину массива по убыванию, а вторую — по возрастанию,
+// используя сортировку простыми вставками.
+
+#include <iostream>
+#include <windows.h>
+#include <random>
+using namespace std;
+
+// Заполнение одномерного массива случайными значениями
+template <typename T>
+void FillArray(T arr[], int size);
+
+// Вывод массива на экран
+template <typename T>
+void PrintArray(T arr[], int size);
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 30;
+    int Array[size];
+    FillArray(Array, size);
+    cout << "Заполненный массив : " << endl;
+    PrintArray(Array, size);   
+}
+
+template <typename T>
+void FillArray(T arr[], int size) {
+    random_device random;
+    for (size_t i = 0; i < size; i++) {
+        arr[i] = random() % (100 + 50) - 50;
+    }
+};
+
+template <typename T>
+void PrintArray(T arr[], int size) {
+    for (size_t i = 0; i < size; i++) {
+        cout << "[" << arr[i] << "] ";
+    }
+    cout << endl;
+};
+
